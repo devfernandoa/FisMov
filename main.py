ddfa58bc79280b74fc4a6fff2bbeeb4b013c356f
devfernandoa <fefealzueta@gmail.com>
2022-09-15 22:49:49 -0300

    coisas

---
+++ main.py
@@ -0,0 +1,7 @@
+from math import *
+def pythagoras(a, b):
+    return sqrt(a**2 + b**2)
+def bhaskara(a, b, c):
+    return (-b + sqrt(b**2 - 4*a*c)) / (2*a)
+
+# Usar Asin, Acos, Atan, Sin, Cos, Tan, etc
